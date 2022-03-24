888f87aa04c06dc96c3bef75c846d2a676719ddb
iqbal8699 <rehandude2214@gmai.com>
2022-03-24 12:12:19 +0530

    iqbal 1st push

---
+++ src/test/java/testyanta_github_practice/CreateOraganizationTest.java
@@ -0,0 +1,11 @@
+package testyanta_github_practice;
+
+import org.testng.annotations.Test;
+
+public class CreateOraganizationTest {
+	@Test
+	public void createOrgization() {
+		System.out.println("iqbal created organization");
+	}
+
+}
